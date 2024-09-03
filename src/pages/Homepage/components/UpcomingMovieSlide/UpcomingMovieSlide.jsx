f1cc8d12ef778fa9ba35c231ecb84c6ca1615bf9
석민영 <149374648+MINYOUNG-SEOK@users.noreply.github.com>
2024-09-03 13:10:38 +0900

    Update UpcomingMovieSlide.jsx

--- src/pages/Homepage/components/UpcomingMovieSlide/UpcomingMovieSlide.jsx
+++ src/pages/Homepage/components/UpcomingMovieSlide/UpcomingMovieSlide.jsx
@@ -1,12 +1,12 @@
-// UpcomingMovieSlide.js
 import React from 'react';
 import { useUpcomingMoviesQuery } from '../../../../hooks/useUpcomingMovies';
 import MovieSlider from '../../../../common/MovieSlider/MovieSlider';
+import Spinner from '../../../../common/Spinner/Spinner';
 
 const UpcomingMovieSlide = () => {
     const { data, isLoading, isError, error } = useUpcomingMoviesQuery();
 
-    if (isLoading) return <div>Loading...</div>;
+    if (isLoading) return <Spinner/>;
     if (isError) {
         console.error('Error:', error);
         return <div>Error fetching data</div>;
