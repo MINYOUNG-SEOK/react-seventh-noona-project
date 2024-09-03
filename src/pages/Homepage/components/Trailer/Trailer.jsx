c117e48f205ec6018885ee526579b32486e1a247
석민영 <149374648+MINYOUNG-SEOK@users.noreply.github.com>
2024-09-03 16:10:44 +0900

    Create Trailer.jsx

---
+++ src/pages/Homepage/components/Trailer/Trailer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useMovieTrailer } from '../../../../hooks/useTrailerMovies';
+import Spinner from '../../../../common/Spinner/Spinner';
+
+const Trailer = ({ movieId }) => {
+    const { data: trailer, error, isLoading } = useMovieTrailer(movieId); 
+
+    if (isLoading) {
+        return <Spinner />
+    }
+
+    if (error) {
+        return <p>예고편을 불러오는 중 오류가 발생했습니다.</p>;
+    }
+
+    if (!trailer) {
+        return <p>예고편을 찾을 수 없습니다.</p>;
+    }
+
+    return (
+        <div className="trailer-container">
+            <iframe
+                width="100%"
+                height="400px"
+                src={`https://www.youtube.com/embed/${trailer.key}`}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+            ></iframe>
+        </div>
+    );
+};
+
+export default Trailer;
