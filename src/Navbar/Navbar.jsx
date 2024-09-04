5c5723bd9a5a492fa0bd8fc74dab833e97bee2c4
석민영 <149374648+MINYOUNG-SEOK@users.noreply.github.com>
2024-09-04 11:51:40 +0900

    Update Navbar.jsx

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
 
   const categories = [
     { href: '/', label: '홈' },
-    { href: '/series', label: '시리즈' },
+    // { href: '/series', label: '시리즈' },
     { href: '/movies', label: '영화' },
     { href: '/my-list', label: '내가 찜한 리스트' },
     { href: '/languages', label: '언어별로 찾아보기' },
