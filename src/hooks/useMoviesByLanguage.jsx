59d066ddb1a750a9d792bfc4b87885fe88cc892c
석민영 <149374648+MINYOUNG-SEOK@users.noreply.github.com>
2024-09-06 17:41:08 +0900

    Update useMoviesByLanguage.jsx

--- src/hooks/useMoviesByLanguage.jsx
+++ src/hooks/useMoviesByLanguage.jsx
@@ -1,28 +1,29 @@
 import { useQuery } from '@tanstack/react-query';
 import api from '../utils/api';
 
-const fetchMoviesByLanguage = async (language) => {
-    try {
-        const response = await api.get(`/discover/movie`, {
-            params: {
-                language: 'ko-KR', 
-                with_original_language: language,
-                sort_by: 'popularity.desc', 
-            },
-        });
-        return response.data.results; 
-    } catch (error) {
-        console.error("Error fetching movies by language", error);
-        throw error;
-    }
+const fetchMoviesByLanguage = async (language, sortOrder) => {
+  try {
+    const response = await api.get(`/discover/movie`, {
+      params: {
+        api_key: process.env.REACT_APP_TMDB_API_KEY,
+        language: 'ko-KR',
+        with_original_language: language,
+        sort_by: sortOrder,
+      },
+    });
+    return response.data.results;
+  } catch (error) {
+    console.error("Error fetching movies by language", error);
+    throw error;
+  }
 };
 
-export const useMoviesByLanguageQuery = (language) => {
-    return useQuery({
-        queryKey: ['moviesByLanguage', language], 
-        queryFn: () => fetchMoviesByLanguage(language), 
-        onSuccess: (data) => console.log('Fetched movies:', data),
-        onError: (error) => console.error('Fetching error:', error),
-        staleTime: 5 * 60 * 1000, 
-    });
+export const useMoviesByLanguageQuery = (language, sortOrder) => {
+  return useQuery({
+    queryKey: ['moviesByLanguage', language, sortOrder],
+    queryFn: () => fetchMoviesByLanguage(language, sortOrder),
+    onSuccess: (data) => console.log('Fetched movies:', data),
+    onError: (error) => console.error('Fetching error:', error),
+    staleTime: 5 * 60 * 1000,
+  });
 };
