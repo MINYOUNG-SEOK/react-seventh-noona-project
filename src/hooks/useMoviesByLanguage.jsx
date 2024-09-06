a26fba36d6e5680b8888229c7356f65d80574579
석민영 <149374648+MINYOUNG-SEOK@users.noreply.github.com>
2024-09-06 18:00:43 +0900

    Update useMoviesByLanguage.jsx

--- src/hooks/useMoviesByLanguage.jsx
+++ src/hooks/useMoviesByLanguage.jsx
@@ -1,27 +1,28 @@
+// src/hooks/useMoviesByLanguage.js
 import { useQuery } from '@tanstack/react-query';
 import api from '../utils/api';
 
-const fetchMoviesByLanguage = async (language, sortOrder) => {
+const fetchMoviesByLanguage = async (language, sortOrder, page) => {
   try {
     const response = await api.get(`/discover/movie`, {
       params: {
-        api_key: process.env.REACT_APP_TMDB_API_KEY,
         language: 'ko-KR',
         with_original_language: language,
         sort_by: sortOrder,
+        page, 
       },
     });
-    return response.data.results;
+    return response.data;
   } catch (error) {
     console.error("Error fetching movies by language", error);
     throw error;
   }
 };
 
-export const useMoviesByLanguageQuery = (language, sortOrder) => {
+export const useMoviesByLanguageQuery = (language, sortOrder, page) => {
   return useQuery({
-    queryKey: ['moviesByLanguage', language, sortOrder],
-    queryFn: () => fetchMoviesByLanguage(language, sortOrder),
+    queryKey: ['moviesByLanguage', language, sortOrder, page], // 페이지를 쿼리 키에 포함
+    queryFn: () => fetchMoviesByLanguage(language, sortOrder, page), // 페이지를 쿼리 함수에 전달
     onSuccess: (data) => console.log('Fetched movies:', data),
     onError: (error) => console.error('Fetching error:', error),
     staleTime: 5 * 60 * 1000,
