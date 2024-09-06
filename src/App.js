46f127203e734b8e725ce3423f4448d711b10d37
석민영 <149374648+MINYOUNG-SEOK@users.noreply.github.com>
2024-09-06 16:45:37 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
 import Homepage from "./pages/Homepage/Homepage";
 import MoviePage from "./pages/Movies/MoviePage";
+import FavoriteMoviesPage from './pages/FavoriteMovies/FavoriteMoviesPage'; 
 import MovieDetailPage from "./pages/MovieDetail/MovieDetailPage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
-
 function App() {
   return (
     <>
@@ -17,6 +17,7 @@ function App() {
           <Route index element={<MoviePage />} />
           <Route path=":id" element={<MovieDetailPage />} />
         </Route>
+        <Route path="favorite-movies" element={<FavoriteMoviesPage />}/>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
